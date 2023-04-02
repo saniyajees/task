1c45246f23d10801610fe7c2e3cfab26e7416f6c
saniyajees <122408424+saniyajees@users.noreply.github.com>
2023-04-02 10:51:16 +0500

    ndfodifobg

---
+++ index.js
@@ -0,0 +1,16 @@
+// var first name = 'Alexenda';
+// var firstchAar = first.name.slice(0,3);
+// console.log(firstchar);
+
+// var firstchar = firstname.charart(5);
+// console.log(firstchar)
+
+// var name = "sara"
+// var lastchar = name.
+for (var text = 0; i < text.length; i++) {
+     if (text.slice(i, i + 12) === "World War II") {
+     text = text.slice(0, 1) + "the Second World War" + text.slice(i + 12);
+     }
+     console.log(text1);
+     }
+    //  console.log(text1);
